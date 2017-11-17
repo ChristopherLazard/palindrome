ba30d188265663ba3174e05190b60cb3ededce10
Christopher Lazard <christopher.l.lazard@gmail.com>
2017-11-17 09:55:47 -0700

    palindrome

---
+++ palindrome.rb
@@ -0,0 +1,35 @@
+def menu
+  puts
+  puts
+  puts "Enter a word to see if it is a palindrome!"
+  puts
+  puts
+  puts "Type 'Quit' to exit."
+  word = gets.strip
+
+  case word
+    when "quit"
+    exit
+    else reverse_string(word)
+  end
+end
+
+def reverse_string(word)
+  indexword = word.split("")
+  reversed = []
+  word.size.times { reversed << indexword.pop }
+  word2 = reversed.join
+  reveal(word2, word)
+end
+
+def reveal(word2, word)
+if word2 == word
+  p 'ITS A PALINDROME'
+  menu
+else
+  p 'ITS NOT A PALINDROME'
+  menu
+end
+end
+
+menu
